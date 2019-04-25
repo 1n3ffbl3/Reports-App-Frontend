7e9843c5bc4666a053f567d8c5a29cca1cb0bc83
1n3ffbl3 <mizgier.m@gmail.com>
2019-04-25 14:02:41 +0200

    Installing Sass StyleSheet. Adding style for Form and Input components. Adding global style

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,16 @@
 /* eslint-disable indent */
 import React from 'react';
+import styles from './Form.module.scss';
 import Input from '../Input/Input';
 
 function Form() {
 	return (
-		<form>
-			<Input />
-		</form>
+		<div className={styles.wrapper}>
+			<h2 className={styles.header}>ADD NEW ITEM</h2>
+			<form className={styles.form}>
+				<Input />
+			</form>
+		</div>
 	);
 }
 
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,23 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
+import styles from './Input.module.scss';
+import PropTypes from 'prop-types';
 
-function Input() {
-	return <input />;
+function Input({ name, label }) {
+	return (
+		<div className={styles.inputWrapper}>
+			<input className={styles.input} id={name} type="text" />
+			<label className={styles.label} htmlFor={name}>
+				{label}
+			</label>
+			<div className={styles.inputBar} />
+		</div>
+	);
 }
+
+Input.propTypes = {
+	name: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+};
 
 export default Input;
