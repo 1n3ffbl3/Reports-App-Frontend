5f0b66e67e72e23ec871763c32dcc23482713321
1n3ffbl3 <mizgier.m@gmail.com>
2019-04-26 11:24:34 +0200

    Creating Report class for reports prop in List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ListItem.module.scss';
 import ListItem from './ListItem';
+import Report from '../../model/Report';
 
 function List({ reports }) {
 	return (
-		<ul>
-			<ListItem />
-		</ul>
+		<>
+			{reports ? (
+				<ul className={styles.wrapper}>
+					{reports.map(report => (
+						<ListItem {...report} key={report.title} />
+					))}
+				</ul>
+			) : (
+				<div />
+			)}
+		</>
 	);
 }
+
+List.propTypes = {
+	reports: PropTypes.arrayOf(PropTypes.oneOf([PropTypes.instanceOf(Report)])).isRequired,
+};
 
 export default List;
